499654b2e33f10441b45fc9755a5966949484d00
416329808 <416329808@qq.com>
2019-04-22 23:34:26 +0800

    修改demo7.2

--- demo7/demo2.py
+++ demo7/demo2.py
@@ -1,7 +1,7 @@
 import datetime
 now = datetime.datetime.now()
-print(now.date())
+print('当前的日期为:', now.date(), end='')
 deita = now .date() - datetime.date(now.year-1, 12, 31)
 week = (deita.days // 7) + 1
 week_days = deita.days % 7 + 1
-print(week, week_days)
+print('是%d的第%d个周的第%d天' %(now.year, week, week_days))
